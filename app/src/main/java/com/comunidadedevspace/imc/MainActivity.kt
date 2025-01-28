36d44cd43cd2410cb242dd0b957359095b4c995e
André Brito Vita <andrebritovita.dev@hotmail.com>
2025-01-28 15:07:46 -0300

    Mostrar mensagem para o usuário preencher os campos vazios.

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,13 +19,24 @@ class MainActivity : AppCompatActivity() {
 
         // Implementando uma ação após o clique do botão.
         btnCalcular.setOnClickListener {
-            val peso: Float = edtPeso.text.toString().toFloat()
-            val altura: Float = edtAltura.text.toString().toFloat()
-        // Cálculo do IMC
-            val altura2: Float = altura * altura
-            val resultado: Float = peso / altura2
-            println(resultado)
-
+            val pesoStr: String = edtPeso.text.toString()
+            val alturaStr: String = edtAltura.text.toString()
+            // Verificação para saber se todos os dados foram passados.
+            if (pesoStr.isEmpty() || alturaStr.isEmpty()){
+                Snackbar.make(
+                    edtPeso, // O componente ao qual o Snackbar será ancorado
+                    "Preencha todos os campos.",
+                    Snackbar.LENGTH_LONG
+                ).show()
+            } else {
+                //Conversão para "Float"
+                val peso: Float = pesoStr.toFloat()
+                val altura: Float = alturaStr.toFloat()
+                // Cálculo do IMC
+                val alturaQ2: Float = altura * altura
+                val resultado: Float = peso / alturaQ2
+                println("André ação do botão $resultado")
+            }
         }
     }
 }
